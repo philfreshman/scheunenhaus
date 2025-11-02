a238558dc00b7b5cc59c5cbefcb20e32005a085e
philfreshman <philipswiezak@gmail.com>
2025-11-02 16:17:00 +0100

    Update biome, reformat project

--- src/utils/useIntersectionObserver.ts
+++ src/utils/useIntersectionObserver.ts
@@ -1,18 +1,21 @@
-export const useIntersectionObserver = (elements: HTMLElement[], onIntersect: (element: HTMLElement) => void) => {
+export const useIntersectionObserver = (
+    elements: HTMLElement[],
+    onIntersect: (element: HTMLElement) => void,
+) => {
     const callback = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
-                onIntersect(entry.target as HTMLElement);
-                observer.unobserve(entry.target);
+                onIntersect(entry.target as HTMLElement)
+                observer.unobserve(entry.target)
             }
-        });
-    };
+        })
+    }
 
     const observer = new IntersectionObserver(callback, {
         threshold: 0.2,
-    });
+    })
 
     elements.forEach((element) => {
-        observer.observe(element);
-    });
-};
+        observer.observe(element)
+    })
+}
